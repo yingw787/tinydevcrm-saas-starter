e9246765d1f839ec87524a1cfc0414f7632bd891
Ying Wang <yingw787.personal@gmail.com>
2020-04-30 15:33:09 -0400

    Add entrypoint.development.sh.

---
+++ services/app/conf/entrypoint.development.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+export BASEDIR='/usr/src/app'
+
+python ${BASEDIR}/manage.py flush --no-input
+python ${BASEDIR}/manage.py migrate
+python ${BASEDIR}/manage.py collectstatic --no-input --clear
+
+exec "$@"
