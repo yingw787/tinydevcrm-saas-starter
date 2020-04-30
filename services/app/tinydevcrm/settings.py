b5d69f84c4adf1e48aa0ef7e1552e02649a7c993
Ying Wang <yingw787.personal@gmail.com>
2020-04-30 15:38:16 -0400

    Update settings.py with environment variable templating.

--- services/app/tinydevcrm/settings.py
+++ services/app/tinydevcrm/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(yxdxq7=0uo)np8w77*d&qqog5^5dsdkg3mxi#-zn@i8rxa24_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS')
 
 
 # Application definition
@@ -75,11 +75,35 @@ WSGI_APPLICATION = 'tinydevcrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get(
+            'SQL_ENGINE',
+            'django.db.backends.sqlite3'
+        ),
+        'NAME': os.environ.get(
+            'SQL_DATABASE',
+            os.path.join(
+                BASE_DIR,
+                'db.sqlite3'
+            )
+        ),
+        'USER': os.environ.get(
+            'SQL_USER',
+            'user'
+        ),
+        'PASSWORD': os.environ.get(
+            'SQL_PASSWORD',
+            'password'
+        ),
+        'HOST': os.environ.get(
+            'SQL_HOST',
+            'localhost'
+        ),
+        'PORT': os.environ.get(
+            'SQL_PORT',
+            '5432'
+        )
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
