2e9f71ca2aa70997f2c101d0171464e351c82761
Ying Wang <yingw787.personal@gmail.com>
2020-05-11 13:55:11 -0400

    Update initdb-pgcron.sh.

--- services/db/docker-entrypoint-initdb.d/initdb-pgcron.sh
+++ services/db/docker-entrypoint-initdb.d/initdb-pgcron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+set -ex
 
 # Perform all actions as $POSTGRES_USER
 export PGUSER="${POSTGRES_USER}"
@@ -10,8 +10,7 @@ export PGDATABASE="${POSTGRES_DB}"
 echo "shared_preload_libraries = 'pg_cron'" >> /var/lib/postgresql/data/postgresql.conf
 echo "cron.database_name = '${POSTGRES_DB}'" >> /var/lib/postgresql/data/postgresql.conf
 
-# Create the 'cron.database' database.
-"${psql[@]}" <<- 'EOSQL'
+# Create the 'pg_cron' PostgreSQL extension.
+PGPASSWORD=$PGPASSWORD psql -U $PGUSER -d $PGDATABASE <<- EOSQL
 CREATE EXTENSION pg_cron;
 EOSQL
-done
