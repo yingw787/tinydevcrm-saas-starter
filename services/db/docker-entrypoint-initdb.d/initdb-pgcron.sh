b6477b11b864270109b26ecd30f333b5a1ca23ce
Ying Wang <yingw787.personal@gmail.com>
2020-05-11 14:29:31 -0400

    Update initdb-pgcron.sh working pg_cron extension creationgit add services/

--- services/db/docker-entrypoint-initdb.d/initdb-pgcron.sh
+++ services/db/docker-entrypoint-initdb.d/initdb-pgcron.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+# Docker entrypoint script for custom extension, loaded as per the Docker Alpine
+# Linux entrypoint instructions (all custom entrypoint scripts in directory
+# /docker-entrypoint-initdb.d).
 
 set -ex
 
@@ -10,7 +14,12 @@ export PGDATABASE="${POSTGRES_DB}"
 echo "shared_preload_libraries = 'pg_cron'" >> /var/lib/postgresql/data/postgresql.conf
 echo "cron.database_name = '${POSTGRES_DB}'" >> /var/lib/postgresql/data/postgresql.conf
 
+# Need to restart the database in order to reload the updated configuration
+# file. This does not terminate the Docker container or cause an infinite loop.
+# No timeout needs to be specified, this script should be synchronous.
+/usr/local/bin/pg_ctl restart -D /var/lib/postgresql/data
+
 # Create the 'pg_cron' PostgreSQL extension.
-PGPASSWORD=$PGPASSWORD psql -U $PGUSER -d $PGDATABASE <<- EOSQL
-CREATE EXTENSION pg_cron;
+PGPASSWORD=$POSTGRES_PASSWORD psql -U $POSTGRES_USER -d $POSTGRES_DB <<- EOSQL
+CREATE EXTENSION IF NOT EXISTS pg_cron;
 EOSQL
