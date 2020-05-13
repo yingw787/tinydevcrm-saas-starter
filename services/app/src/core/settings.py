5054cd292e3fc46d7ae79acd52b30d19a891d3f9
Ying Wang <yingw787.personal@gmail.com>
2020-05-13 16:47:55 -0400

    Update settings.py add failover for DJANGO_ALLOWED_HOSTS to all.

--- services/app/src/core/settings.py
+++ services/app/src/core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # mapping instead.
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split()
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split()
 
 
 # Application definition
