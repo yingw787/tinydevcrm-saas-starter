854d92d1b5cdf5be8ac96274034a3876ddb4a5e3
Ying Wang <yingw787.personal@gmail.com>
2020-05-13 16:20:03 -0400

    Update models.py add basic. File model.

--- services/app/src/concrete_data/models.py
+++ services/app/src/concrete_data/models.py
@@ -1,3 +1,25 @@
+"""
+Models for managing file uploads.
+"""
+
+import os
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+DATA_DUMP_DIRECTORY = os.path.join(
+    settings.MEDIA_ROOT,
+    'data-dumps'
+)
+
+
+class File(models.Model):
+    """
+    Model for files uploaded to TinyDevCRM.
+    """
+    file_id = models.AutoField(primary_key=True)
+    file = models.FileField(upload_to=DATA_DUMP_DIRECTORY)
+
+    def __str__(self):
+        return str(self.file.name)
