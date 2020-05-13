e6cec60a83eb8c4b4628e3897867ad5efc23184c
Ying Wang <yingw787.personal@gmail.com>
2020-05-13 11:33:00 -0400

    Update test.sh.

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 # Register a new test user.
-curl --header "Content-Type: application/json" -X POST http://localhost:1337/v1/auth/users/register/ --data '{"primary_email": "me@yingw787.com", "password": "test1234"}'
+curl --header "Content-Type: application/json" -X POST http://localhost:1337/v1/auth/users/register/ --data '{"primary_email": "me@yingw787.com", "password": "test1234"}' || true
 
-#
+# Obtain a JSON web token.
+RESPONSE=$(curl --header "Content-Type: application/json" -X POST http://localhost:1337/v1/auth/tokens/obtain/ --data '{"primary_email": "me@yingw787.com", "password": "test1234"}')
+
+echo "Response is: " $RESPONSE
+
+REFRESH=$(echo $RESPNOSE | jq -r ".refresh")
+ACCESS=$(echo $RESPONSE | jq -r ".access")
+
+echo "Refresh is: " $REFRESH
+echo "Access is: " $ACCESS
