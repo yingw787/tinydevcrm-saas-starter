b30d133745a3dabfc2b69b67e0fd79b02d148059
Ying Wang <yingw787.personal@gmail.com>
2020-05-13 18:12:02 -0400

    Update views.py working POST request for data upload.

--- services/app/src/concrete_data/views.py
+++ services/app/src/concrete_data/views.py
@@ -37,13 +37,30 @@ class FileUploadView(APIView):
     )
 
     def post(self, request, *args, **kwargs):
+        """
+        Handles the HTTP POST request.
+
+        NOTE: These keys, such as 'data' and 'file', are very particular to the
+        underlying models and serializers. Do not change without testing in
+        development.
+        """
+
         file_serializer = serializers.FileSerializer(
-            data=request.data
+            # Use the form key 'file=' in order to send binary files as part of
+            # a multipart/form-data request.
+            #
+            # Example: curl --header "Content-Type: multipart/form-data"
+            # --header "Authorization: JWT $JWT_ACCESS_TOKEN" -X POST  -F
+            # 'file=@"/path/to/sample.csv"'
+            # http://localhost:8000/v1/data/upload/
+            data={
+                'file': request.data['file']
+            }
         )
         if file_serializer.is_valid():
             file_serializer.save()
             return Response(
-                file_serializer.file,
+                file_serializer.data,
                 status=status.HTTP_201_CREATED
             )
         else:
