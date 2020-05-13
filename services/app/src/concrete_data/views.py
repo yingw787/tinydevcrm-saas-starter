76ee72ad6b06d15f5432f0bee7c13ace5898c766
Ying Wang <yingw787.personal@gmail.com>
2020-05-13 16:44:07 -0400

    Update views register fileuploadview.

--- services/app/src/concrete_data/views.py
+++ services/app/src/concrete_data/views.py
@@ -3,8 +3,11 @@ Concrete data service custom views.
 """
 
 from rest_framework import status
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from . import serializers
 
 
 class TestProtectedView(APIView):
@@ -22,3 +25,29 @@ class TestProtectedView(APIView):
             },
             status=status.HTTP_200_OK
         )
+
+
+class FileUploadView(APIView):
+    """
+    Upload files via API.
+    """
+    parser_classes = (
+        MultiPartParser,
+        FormParser,
+    )
+
+    def post(self, request, *args, **kwargs):
+        file_serializer = serializers.FileSerializer(
+            data=request.data
+        )
+        if file_serializer.is_valid():
+            file_serializer.save()
+            return Response(
+                file_serializer.file,
+                status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                file_serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
