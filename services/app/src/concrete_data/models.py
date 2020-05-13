99ca41e536c5d3f3d1932724308befb1eafd3f27
Ying Wang <yingw787.personal@gmail.com>
2020-05-13 16:43:36 -0400

    Update models.py for concrete data.

--- services/app/src/concrete_data/models.py
+++ services/app/src/concrete_data/models.py
@@ -8,18 +8,16 @@ from django.conf import settings
 from django.db import models
 
 
-DATA_DUMP_DIRECTORY = os.path.join(
-    settings.MEDIA_ROOT,
-    'data-dumps'
-)
-
-
 class File(models.Model):
     """
     Model for files uploaded to TinyDevCRM.
     """
     file_id = models.AutoField(primary_key=True)
-    file = models.FileField(upload_to=DATA_DUMP_DIRECTORY)
+    file = models.FileField(
+        upload_to='concrete-data',
+        blank=False,
+        null=False
+    )
 
     def __str__(self):
         return str(self.file.name)
